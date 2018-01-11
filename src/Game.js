6e2a0229a7af745aaf94394431f6d4e128982732
unknown <ahmedm@GD-02-007.ovrlod.pk>
2018-01-11 18:32:30 +0500

    target generation

--- src/Game.js
+++ src/Game.js
@@ -7,36 +7,63 @@ class Game extends Component {
         failed:0,
         total:0,
         bricksVals:[],
+        target:null,
         selectedBricks:[]
     }
 
     componentDidMount() {
+        // target number
+        const target = Math.floor((Math.random()*27) + 1);
+
         // random Bricks Values
         const bricksVals = [];
-        for (let i = 0; i < 9; i++) {
-            let val = Math.floor(Math.random() * 20);
-            bricksVals.push(val);
+        for (let i = 0; i < 20; i++) {
+            let t = this.checkDuplicate(bricksVals);        
+            bricksVals.push(t);
+            if(bricksVals.length == 9) break;
         }
+
         this.setState({
-            bricksVals
+            bricksVals,
+            target
         });
     }
 
-    revealBrick = () => {
-        this.setState();
+    checkDuplicate = (bricksVals) => {
+        let val = Math.floor((Math.random() * 15) + 1);
+        let na = bricksVals.find(item => item === val);
+        console.log(na);
+        if(na){
+            console.log('if');
+            this.checkDuplicate(bricksVals);
+        }
+        else{
+            console.log(na);
+            return val;
+        }
+    }
+
+
+    revealBrick = (i) => {
+        const clickedVal = this.state.bricksVals[i];
+        console.log(clickedVal);
     }
 
     render() {
-        const { bricksVals } = this.state; 
+        const { bricksVals, target } = this.state; 
         return(
-            <div className="wrapper">
-                {
-                    bricksVals.map(brick => 
-                        <div 
-                            className="brick"
-                            onClick={this.revealBrick}
-                        >{brick}</div>)
-                }
+            <div className="content">
+                <h2>Your Target: <span>{target}</span></h2>
+                <div className="wrapper">
+                    {
+                        bricksVals.map((brick, index) =>
+                            <div
+                                key={brick}
+                                className="brick"
+                                onClick={() => this.revealBrick(index)}
+                            >{brick}</div>)
+                    }
+                </div>
             </div>
         );
     }
